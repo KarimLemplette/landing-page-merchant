b68286efe6453d77a19a94e5f76a901e3fbdb179
Karim <karim@lemplette.com>
2022-02-17 15:59:26 +0100

    Responsive mobile

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -9,24 +9,28 @@ import { MDBContainer, MDBRow, MDBCol } from 'mdb-react-ui-kit';
 function Header() {
     return (
         <div style={{backgroundImage:`url(${background})`}}>
-            <MDBContainer breakpoint="md">
+            <MDBContainer breakpoint="md" className='responsive-container-header'>
                 <MDBRow>
                     <Navbar></Navbar>
-                        <MDBCol size='md' className='col-md-6'>
-                            <h1 className="header-h2">L’<span style={{color: '#ffc107'}}>e</span>.Commerce Citoyen</h1>
-                            <p className="header-p">
+                </MDBRow>
+            </MDBContainer>
+            <MDBContainer breakpoint="md" className='container-fluid'>
+                <MDBRow className='d-flex align-items-center'>
+                        <MDBCol size='md' className='col-md-6 order-sm-first order-last'>
+                            <h1 className="header-h2 responsive-header-h2">L’<span style={{color: '#ffc107'}}>e</span>.Commerce Citoyen</h1>
+                            <p className="header-p responsive-header-p">
                                 Des centaines de commerces proches de
                                 chez vous ont déjà en stock le produit que vous souhaitez commander, demandez-leur
                                 directement grâce à Lemplette
                             </p>
                         </MDBCol>
-                        <MDBCol size='md' className='col-md-4 mt-5'>
+                        <MDBCol size='md' className='col-md-5 mt-5 responsive-image-header-width'>
                             <img src={logo} className='img-fluid'/>
                         </MDBCol>
                 </MDBRow>
                 <MDBRow>
                     <MDBCol size='md' className='col-md-12 mt-5 mb-5 text-center'>
-                        <MDBBtn rounded className='mx-2 btn btn-warning btn-lg'>
+                        <MDBBtn rounded className='mx-2 btn btn-warning btn-lg font-weight-bold'>
                             Déposer une demande
                         </MDBBtn>
                     </MDBCol>
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -5,10 +5,10 @@ import './Navbar.css';
 function Navbar() {
     return (
         <MDBContainer breakpoint="md" className='navbar-buttons'>
-            <MDBBtn rounded className='mx-2 mt-3' color='secondary'>
+            <MDBBtn rounded className='mx-2 mt-3 responsive-button-width' color='secondary'>
                 Les commercants
             </MDBBtn>
-            <MDBBtn rounded className='mx-2 mt-3' color='warning'>
+            <MDBBtn rounded className='mx-2 mt-3 responsive-button-width' color='warning'>
                 S'inscrire/Mon compte
             </MDBBtn>
         </MDBContainer>
